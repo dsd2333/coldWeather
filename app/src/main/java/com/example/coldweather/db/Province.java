d11663b8e8602fa3d22bb02095604d9864ac109f
dsd961028 <18792683082@163.com>
2018-11-19 16:12:26 +0800

    加入创建数据库和表的配置

---
+++ app/src/main/java/com/example/coldweather/db/Province.java
@@ -0,0 +1,38 @@
+package com.example.coldweather.db;
+
+import org.litepal.crud.LitePalSupport;
+
+
+/**
+ * Created by 杜松阳 on 2018/11/19.
+ */
+
+public class Province extends LitePalSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
